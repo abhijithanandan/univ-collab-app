d6ced9214b5f382c290124965a347895f51e9d5f
Abhijith Anandakrishnan <abhijithananthan@gmail.com>
2020-07-28 18:21:49 +0530

    Added profile image cropping feature

--- android/app/src/main/java/in/co/freshstart/android/SetupActivity.java
+++ android/app/src/main/java/in/co/freshstart/android/SetupActivity.java
@@ -1,10 +1,12 @@
 package in.co.freshstart.android;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -17,6 +19,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -33,6 +37,7 @@ public class SetupActivity extends AppCompatActivity {
     private DatabaseReference UserRef;
 
     String currentUserID;
+    final static int Gallery_Pick = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +61,37 @@ public class SetupActivity extends AppCompatActivity {
                 SaveAccountSetupInformation();
             }
         });
+
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, Gallery_Pick);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == Gallery_Pick && resultCode ==RESULT_OK && data != null){
+            Uri ImageUri = data.getData();
+
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+            if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                    if(requestCode == RESULT_OK){
+                        Uri resultUri = result.getUri();
+                    }
+            }
+        }
     }
 
     private void SaveAccountSetupInformation() {
